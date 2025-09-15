3c13cb1f33de7095893e24f4d76c43cb918c9190
farooq-teqniqly <farooq@teqniqly.com>
2025-09-15 13:22:23 -0700

    chore: Create Category model; Categories db table; Seed table with Categories.

--- YumBlazor/Data/ApplicationDbContext.cs
+++ YumBlazor/Data/ApplicationDbContext.cs
@@ -3,7 +3,22 @@ using Microsoft.EntityFrameworkCore;
 
 namespace YumBlazor.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+  public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : IdentityDbContext<ApplicationUser>(options)
+  {
+    public DbSet<Category> Categories => Set<Category>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
     {
+      base.OnModelCreating(builder);
+
+      builder
+        .Entity<Category>()
+        .HasData(
+          new Category { Id = 1, Name = "Appetizer" },
+          new Category { Id = 2, Name = "Entree" },
+          new Category { Id = 3, Name = "Dessert" }
+        );
     }
+  }
 }
