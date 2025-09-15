032475086ef6afae8aae0c827e39cfb46088643b
farooq-teqniqly <farooq@teqniqly.com>
2025-09-15 14:17:45 -0700

    chore: Show list of Categories from CategoryService.

--- YumBlazor/Program.cs
+++ YumBlazor/Program.cs
@@ -4,69 +4,86 @@ using Microsoft.EntityFrameworkCore;
 using YumBlazor.Components;
 using YumBlazor.Components.Account;
 using YumBlazor.Data;
+using YumBlazor.Services;
 
 namespace YumBlazor
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+      var builder = WebApplication.CreateBuilder(args);
+
+      // Add services to the container.
+      builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+      builder.Services.AddCascadingAuthenticationState();
+      builder.Services.AddScoped<IdentityUserAccessor>();
+      builder.Services.AddScoped<IdentityRedirectManager>();
+      builder.Services.AddScoped<
+        AuthenticationStateProvider,
+        IdentityRevalidatingAuthenticationStateProvider
+      >();
+
+      builder
+        .Services.AddAuthentication(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+          options.DefaultScheme = IdentityConstants.ApplicationScheme;
+          options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+        })
+        .AddIdentityCookies();
 
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
+      var connectionString =
+        builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+      builder.Services.AddDbContext<ApplicationDbContext>(options =>
+      {
+        options.UseSqlServer(connectionString);
 
-            builder.Services.AddCascadingAuthenticationState();
-            builder.Services.AddScoped<IdentityUserAccessor>();
-            builder.Services.AddScoped<IdentityRedirectManager>();
-            builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
-
-            builder.Services.AddAuthentication(options =>
-                {
-                    options.DefaultScheme = IdentityConstants.ApplicationScheme;
-                    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-                })
-                .AddIdentityCookies();
-
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-            builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddSignInManager()
-                .AddDefaultTokenProviders();
-
-            builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseMigrationsEndPoint();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAntiforgery();
-
-            app.MapStaticAssets();
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
-
-            // Add additional endpoints required by the Identity /Account Razor components.
-            app.MapAdditionalIdentityEndpoints();
-
-            app.Run();
+        if (builder.Environment.IsDevelopment())
+        {
+          options.EnableSensitiveDataLogging();
         }
+      });
+      builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+      builder
+        .Services.AddIdentityCore<ApplicationUser>(options =>
+          options.SignIn.RequireConfirmedAccount = true
+        )
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddSignInManager()
+        .AddDefaultTokenProviders();
+
+      builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+      builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+      var app = builder.Build();
+
+      // Configure the HTTP request pipeline.
+      if (app.Environment.IsDevelopment())
+      {
+        app.UseMigrationsEndPoint();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseAntiforgery();
+
+      app.MapStaticAssets();
+      app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+      // Add additional endpoints required by the Identity /Account Razor components.
+      app.MapAdditionalIdentityEndpoints();
+
+      app.Run();
     }
+  }
 }
